2f090031bb5f1f23f105e0201459ba6b88681f0b
Keysamm <zabrodskyi71011@gmail.com>
2021-03-25 11:20:17 +0200

    correct server static

--- server.js
+++ server.js
@@ -59,9 +59,6 @@ app.use('/api', postRoute)
 
 // Server static assets in production
 if (process.env.NODE_ENV === 'production') {
-  //Set static folder
-  app.use(express.static('client/build'))
-
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'index.html'))
   })
